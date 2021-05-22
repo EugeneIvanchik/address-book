7fc881498f8601f498d112fb199beb0af28c97cd
EugeneIvanchik <EugeneIvanchik@gmail.com>
2021-05-22 22:37:30 +0300

    add contact class is added

---
+++ address book/Add Contact/ContactData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace address_book
+{
+    public class ContactData
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public ContactData(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+    }
+}
