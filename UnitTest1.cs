b32f512a5f5851693c4828b87c5636a6ed749aff
EugeneIvanchik <EugeneIvanchik@gmail.com>
2021-05-22 18:12:45 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace address_book
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
